751af9f9a8af68883963fbfe7cf969fde08b6a49
James Rogers <james@safepointapp.com>
2020-06-06 16:18:36 +0100

    Removed forgotten debug code

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,4 @@
 #!/bin/sh -l
-
-GITHUB_EVENT_PATH=/Users/jamesrogers/Desktop/file.json
-cat $GITHUB_EVENT_PATH
 mentioned_users=$(cat $GITHUB_EVENT_PATH | jq '.pull_request.requested_reviewers | map("@" + .login) | join(" ")')
 echo "Mentioned users: $mentioned_users"
 
