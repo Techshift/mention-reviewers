f023f6464dacd714076136e456c560ec5a8e65f7
James Rogers <james@safepointapp.com>
2020-06-06 16:16:10 +0100

    Fixed bug where login was not indexed properly

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,8 @@
 #!/bin/sh -l
 
+GITHUB_EVENT_PATH=/Users/jamesrogers/Desktop/file.json
 cat $GITHUB_EVENT_PATH
-mentioned_users=$(cat $GITHUB_EVENT_PATH | jq '.pull_request.requested_reviewers | map("@" + .) | join(" ")')
+mentioned_users=$(cat $GITHUB_EVENT_PATH | jq '.pull_request.requested_reviewers | map("@" + .login) | join(" ")')
 echo "Mentioned users: $mentioned_users"
 
 echo ::set-output name=mentions::$mentioned_users
