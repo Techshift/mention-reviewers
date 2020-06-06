f5718ef9058b5565ac8200dfdfd3f7cf08858781
James Rogers <james@safepointapp.com>
2020-06-06 16:02:22 +0100

    Added test output

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,6 @@
 #!/bin/sh -l
 
+cat $GITHUB_EVENT_PATH
 mentioned_users=$(cat $GITHUB_EVENT_PATH | jq '.pull_request.requested_reviewers | map("@" + .) | join(" ")')
 echo "Mentioned users: $mentioned_users"
 
